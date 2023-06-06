43f0aad5aa1795cf9b851d9eac0c3d572b20fa35
phaishuk <xomas@ukr.net>
2023-06-06 23:33:03 +0300

    prepare task requirements file to track tasks

--- app/settings.py
+++ app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qdkozv74r^@)vfyon#xz)cf%(pn72bnuzq8h1&fwi-nch=*uy)"
+SECRET_KEY = (
+    "django-insecure-qdkozv74r^@)vfyon#xz)cf%(pn72bnuzq8h1&fwi-nch=*uy)"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
