6dbff7e8a31b33894386077c8b0b1266173a2ad6
phaishuk <xomas@ukr.net>
2023-06-27 17:32:57 +0300

    configure python-dotenv, update requirements.txt.txt

--- app/settings.py
+++ app/settings.py
@@ -1,19 +1,16 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-qdkozv74r^@)vfyon#xz)cf%(pn72bnuzq8h1&fwi-nch=*uy)"
-)
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "") != "True"
 
 if DEBUG:
     import mimetypes
